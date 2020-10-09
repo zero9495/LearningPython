1637dcbd045f560e151b29f9a70153e6e83222b7
zero9495 <zero9495@gmail.com>
2020-10-09 15:36:47 +0300

    Improving exercise 6
    Functions 'count' and 'cycle' moved to single 'for'

--- exercise6.py
+++ exercise6.py
@@ -4,24 +4,11 @@
 	Author: Aleksandr Plyukhin	
 '''
 
-# a
-
 from itertools import count
-
-for el in count(7):
-    if el > 15:
-        break
-    else:
-        print(el)
-
-
-# b
-
 from itertools import cycle
 
-с = 0
-for el in cycle("ABC"):
-    if с > 10:
-        break
-    print(el)
-    с += 1
+my_count = count(7)
+my_cycle = cycle("ABC")
+
+for _ in range(5):
+    print("(my_count, my_cycle) = ({},{})".format(next(my_count), next(my_cycle)))
